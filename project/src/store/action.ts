ab24a33afec422438d0292c94649ac2c9af7ec79
adakhannn <osorovadahan04@gmail.com>
2022-11-24 18:38:34 +0600

    7.16. Добро пожаловать, или посторонним вход воспрещён (часть 1)

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,8 +1,11 @@
 import {createAction} from '@reduxjs/toolkit';
 import {City, Offer, Offers} from '../types/offer';
+import {AppRoute, AuthorizationStatus} from '../const';
 
 export const loadOffers = createAction<Offers>('data/loadOffers');
 export const setError = createAction<string | null>('data/setError');
+export const setOffersDataLoadingStatus = createAction<boolean>('data/setOffersDataLoadingStatus');
 export const changeCity = createAction<{city: City}>('offers/changeCity');
 export const hoverOffer = createAction<{offer: null | Offer}>('offers/hoverOffer');
-export const setOffersDataLoadingStatus = createAction<boolean>('data/setOffersDataLoadingStatus');
+export const redirectToRoute = createAction<AppRoute>('offers/redirectToRoute');
+export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
