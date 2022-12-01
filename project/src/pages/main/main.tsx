aae35cce9b05890b5b3426d81f78f29a6a3e238a
adakhannn <osorovadahan04@gmail.com>
2022-12-01 23:23:57 +0600

    8.14. Оптимизируй это (Часть 2)

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -4,8 +4,12 @@ import Map from '../../components/map/map';
 import Header from '../../components/header/header';
 import Sorter from '../../components/sorter/sorter';
 import Title from '../../components/title/title';
+import {useAppSelector} from '../../hooks';
+import {getOffers} from '../../store/offers-data/selectors';
+import NoPlaces from '../../components/no-places/no-places';
 
 function Main(): JSX.Element {
+  const offers = useAppSelector(getOffers);
   return (
     <div className="page page--gray page--main">
       <Header />
@@ -16,21 +20,35 @@ function Main(): JSX.Element {
             <LocationList />
           </section>
         </div>
-        <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <Title />
-              <Sorter />
-              <CardsList />
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map">
-                <Map />
+        {offers.length > 0
+          ?
+          <div className="cities">
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <Title />
+                <Sorter />
+                <CardsList />
               </section>
+              <div className="cities__right-section">
+                <section className="cities__map map">
+                  <Map />
+                </section>
+              </div>
             </div>
           </div>
-        </div>
+          :
+          <div className="cities">
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <NoPlaces />
+                </div>
+              </section>
+              <div className="cities__right-section"></div>
+            </div>
+          </div>}
       </main>
     </div>
   );
